eb066dd8dd6bfa007ecf4e2f9a36e4a3e1245e6d
Msdph <99562192+Msdph@users.noreply.github.com>
2022-07-20 18:09:15 +0430

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -214,11 +214,11 @@ async def doc(bot,update):
     await ms.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....")
     c_time = time.time() 
     try:
-	    #print(f"{file_path}\n\n{new_filename}")
+	print(f"{file_path}\n\n{new_filename}")
         checkftp(new_filename)
         with open(path, "rb") as file:
             ftp.storbinary(f"STOR ./{new_filename}/{fileeeeeeeeeeeeeeename}", file)
-        await update.reply_text(f"UPLOAD COPLETE \n\nhttps://s2.kenzodl.xyz/mas/{new_filename}")
+        await update.reply_text(f"UPLOAD COPLETE \n\nhttps://s2.kenzodl.xyz/{new_filename}/{new_filename}")
         if type == "document":
             await bot.send_document(
                 update.message.chat.id,

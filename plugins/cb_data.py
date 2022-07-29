f7cb7271f6e958942aa0fdbdc611d918e873a19f
Msdph <99562192+Msdph@users.noreply.github.com>
2022-07-29 23:48:12 +0430

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -10,7 +10,7 @@ from PIL import Image
 import time
 import ftplib
 
-FTP_HOST = "130.185.79.172"
+FTP_HOST = "194.147.142.52"
 FTP_USER = "pz14205"
 FTP_PASS = "12345678"
 # connect to the FTP server
@@ -24,7 +24,7 @@ def handle(sizeWritten,totalSize):
     print ("%s percent complete" %str(sizeWritten / totalSize))
 
 def upftp(path,folder,filename):
-    ftp = ftplib.FTP("130.185.79.172")
+    ftp = ftplib.FTP("194.147.142.52")
     ftp.login("pz14205", "12345678")
     f = open(f"{path}", "rb")
     ftp.cwd(f'/domains/pz14205.parspack.net/public_html/{folder}')
